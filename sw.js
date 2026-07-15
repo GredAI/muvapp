8efd7c36cf3d270fb9062ebbf57c955507918a5e
Glauco Dapporto <glauco@MacBook-Air-di-Glauco.local>
2026-07-15 15:46:00 +0200

    Fix flusso serie, reset, pausa recupero, consigli collassabili

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'muvapp-v2';
+const CACHE = 'muvapp-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
